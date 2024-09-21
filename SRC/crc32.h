7b20a0f589653294a9c4eae3b9a59c1a01ec9fd1
JaneDoe649 <jane.doe62001@gmail.com>
2024-09-21 14:20:29 +0200

    CRC32 patch

--- SRC/crc32.h
+++ SRC/crc32.h
@@ -1,7 +1,8 @@
 #ifndef __CRC32_H
 #define __CRC32_H
+#include <stdint.h>
 
 unsigned long crc32_buf(const void *buf, long len);
-unsigned long crc32_file(const char *filename);
+uint32_t  crc32_file(const char *filename);
 
 #endif
